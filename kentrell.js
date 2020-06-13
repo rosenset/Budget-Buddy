1f4c00ae0cb86161f74b080d82eb2924aa3ae794
Alice Griffin <alicegriffin@alices-air.attlocal.net>
2020-06-13 12:38:22 -0400

    javascript submit button

--- kentrell.js
+++ kentrell.js
@@ -1,8 +1,32 @@
-function inPutBudget(budget) {
-    const budgetButton = document.getElementById('submit-your-budget');
-    budgetButton.addEventListener("click", () => {
-        
-    document.getElementById('what-is-your-budget').data.get('budgetinput');
-    parseInt(userBudget)
-    })
+class BudgetBuddy {
+    constructor()   {
+    this.budgetSubmitButton = document.getElementById('submit-your-budget');
+    this.userBudgetInput = parseInt(document.getElementById('what-is-your-budget').value);
+    this.userBudgetDisplay = document.getElementById("userBudget");
+    this.amountSpentDisplay = document.getElementById("amountSpent");
+    this.balanceDisplay = document.getElementById("balance");
+    }
+
+    submitBudgetInput() {
+    const budgetAmout = this.userBudgetInput;
+        if (this.userBudgetInput < 0) {
+            alert("Your budget must be positive. Try again.");
+        } else {
+        this.userBudgetDisplay.innerText = "$" + budgetAmout; 
+        }
+    }
+}
+
+const newBudget = new BudgetBuddy() 
+
+function budgetFunctions() {
+const budgetSubmitButton = document.getElementById('submit-your-budget');
+//const userBudgetInput = parseInt(document.getElementById('what-is-your-budget').value);
+//const userBudgetDisplay = document.getElementById("userBudget");
+
+budgetSubmitButton.addEventListener("submit", () => {
+    event.preventDefault();
+    newBudget.submitBudgetInput();
+})
+
 }
